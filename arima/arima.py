a2ff4a6289af1a2010b6a959f62fe231afd0dbdd
Yusuf <yusufsariyildiz35@gmail.com>
2019-04-04 11:27:44 +0300

    medium

---
+++ arima/arima.py
@@ -0,0 +1,33 @@
+from pandas import read_csv
+from pandas import datetime
+from matplotlib import pyplot
+from statsmodels.tsa.arima_model import ARIMA
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+
+def parser(x):
+	return datetime.strptime('19'+x, '%Y-%m')
+
+series = read_csv('dataset1.csv', header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
+X = series.values
+size = int(len(X) * 0.66)
+train, test = X[0:size], X[size:len(X)]
+history = [x for x in train]
+predictions = list()
+for t in range(len(test)):
+	model = ARIMA(history, order=(5,1,0))
+	model_fit = model.fit(disp=0)
+	output = model_fit.forecast()
+	yhat = output[0]
+	predictions.append(yhat)
+	obs = test[t]
+	history.append(obs)
+	print('predicted=%f, expected=%f' % (yhat, obs))
+error = mean_squared_error(test, predictions)
+
+rmse=sqrt(error)
+print('RMSE: %.3f' % rmse)
+# plot
+pyplot.plot(test)
+pyplot.plot(predictions, color='red')
+pyplot.show()
